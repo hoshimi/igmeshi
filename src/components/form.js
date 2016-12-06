c582aad02fb341641938f8d16cebfe0b4745650f
hsimyu <samisigena.sazanami@gmail.com>
2016-12-06 15:30:19 +0900

    Separated components dir.

---
+++ src/components/form.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {FormControl, ControlLabel, FormGroup} from 'react-bootstrap';
+
+class DetailForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            FoodTitle: "",
+            Effect1: "",
+            Effect2: "",
+            Effect3: "",
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(key, e) {
+        this.setState({[key]: e.target.value})
+    }
+
+    render() {
+        return (
+            <FormGroup controlId="meshiForm">
+            <ControlLabel>料理名</ControlLabel>
+            <FormControl type="text" value={this.state.FoodTitle} onChange={this.handleChange.bind(this, "FoodTitle")}/>
+            <ControlLabel>効果1</ControlLabel>
+            <FormControl type="text" value={this.state.Effect1} onChange={this.handleChange.bind(this, "Effect1")}/>
+            <ControlLabel>効果2</ControlLabel>
+            <FormControl type="text" value={this.state.Effect2} onChange={this.handleChange.bind(this, "Effect2")}/>
+            <ControlLabel>効果3</ControlLabel>
+            <FormControl type="text" value={this.state.Effect3} onChange={this.handleChange.bind(this, "Effect3")}/>
+            </FormGroup>
+            )
+    }
+}
+
+export default DetailForm;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Grid, Col, Row, FormControl, ControlLabel, FormGroup} from 'react-bootstrap';
+import {Grid, Col, Row} from 'react-bootstrap';
+import DetailForm from './components/form.js';
 // import ReactCanvas from 'react-canvas';
 // import {Surface, Image, Text} from 'react-canvas';
 
@@ -18,39 +19,6 @@ class MainContainer extends React.Component {
                </Row>
                </Grid>
                </div>
-    }
-}
-
-class DetailForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            FoodTitle: "",
-            Effect1: "",
-            Effect2: "",
-            Effect3: "",
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(key, e) {
-        this.setState({[key]: e.target.value})
-    }
-
-    render() {
-        return (
-            <FormGroup controlId="meshiForm">
-            <ControlLabel>料理名</ControlLabel>
-            <FormControl type="text" value={this.state.FoodTitle} onChange={this.handleChange.bind(this, "FoodTitle")}/>
-            <ControlLabel>効果1</ControlLabel>
-            <FormControl type="text" value={this.state.Effect1} onChange={this.handleChange.bind(this, "Effect1")}/>
-            <ControlLabel>効果2</ControlLabel>
-            <FormControl type="text" value={this.state.Effect2} onChange={this.handleChange.bind(this, "Effect2")}/>
-            <ControlLabel>効果3</ControlLabel>
-            <FormControl type="text" value={this.state.Effect3} onChange={this.handleChange.bind(this, "Effect3")}/>
-            </FormGroup>
-            )
     }
 }
 
