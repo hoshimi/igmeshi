efa10e6aacc432105968ec51f5265e83289d11ab
hsimyu <samisigena.sazanami@gmail.com>
2016-12-10 14:59:12 +0900

    Amazon商品リンクを追加.

--- index.php
+++ index.php
@@ -37,9 +37,8 @@
         </div>
         <script src="./build/index.js"></script>
         <div class="footerAdv">
-            <iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=chubounosuk00-22&o=9&p=8&l=as1&m=amazon&f=ifr&ref=qf_sp_asin_til&asins=B01DN9WFB6" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
-
-            <iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS1=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=chubounosuk00-22&o=9&p=8&l=as1&m=amazon&f=ifr&ref=qf_sp_asin_til&asins=B01AS5EGXS" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+            <h3>FFXV 関連商品</h3>
+            <?php include("./associate.html"); ?>
         </div>
     </body>
 </html>
